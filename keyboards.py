eded1165080a442383e77414828cfac955240526
77agbox <viktoria.metod@gmail.com>
2026-02-27 14:49:09 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,38 +1,15 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 def main_menu(is_admin=False):
     """
     Главное меню. Если администратор, то добавляется кнопка для админ-панели.
     """
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="🎓 Кружки", callback_data="m_clubs")],
-            [InlineKeyboardButton(text="🎯 Пакетные туры", callback_data="m_package")],
-            [InlineKeyboardButton(text="🎨 Мастер-классы", callback_data="m_master")]
-        ]
-    )
-    
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.insert(InlineKeyboardButton(text="🎓 Кружки", callback_data="m_clubs"))
+    keyboard.insert(InlineKeyboardButton(text="🎯 Пакетные туры", callback_data="m_package"))
+    keyboard.insert(InlineKeyboardButton(text="🎨 Мастер-классы", callback_data="m_master"))
+
     if is_admin:
-        keyboard.add(InlineKeyboardButton(text="⚙ Админ-панель", callback_data="admin_panel"))
+        keyboard.insert(InlineKeyboardButton(text="⚙ Админ-панель", callback_data="admin_panel"))
 
     return keyboard
-
-def bottom_kb(is_admin=False):
-    """
-    Нижняя клавиатура (Reply).
-    Всегда:
-    - Начать заново
-    - Написать в поддержку
-    """
-    builder = ReplyKeyboardBuilder()
-
-    builder.button(text="🏠 Начать заново")
-    builder.button(text="✉ Написать в поддержку")
-    
-    if is_admin:
-        builder.button(text="⚙ Админ-панель")
-
-    builder.adjust(1)
-
-    return builder.as_markup(resize_keyboard=True)
