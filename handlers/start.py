0321555d52fd5275d3bf6e3015fee01d74494266
77agbox <viktoria.metod@gmail.com>
2026-02-27 14:41:38 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -4,17 +4,15 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardRemove
 
-from config import ADMIN_ID
 from database import add_subscriber, check_subscription
 from keyboards import bottom_kb, main_menu
+from config import ADMIN_ID
 
 router = Router()
-
 
 class MasterForm(StatesGroup):
     waiting_name = State()  # Состояние для ввода имени
     waiting_phone = State()  # Состояние для ввода телефона
-
 
 @router.message(Command("start"))
 async def cmd_start(message: types.Message, state: FSMContext):
@@ -51,7 +49,6 @@ async def cmd_start(message: types.Message, state: FSMContext):
         ),
     )
 
-
 @router.message(lambda m: m.text == "🏠 Начать заново")
 async def restart(message: types.Message, state: FSMContext):
     await state.clear()
@@ -59,35 +56,3 @@ async def restart(message: types.Message, state: FSMContext):
         "Выберите раздел:",
         reply_markup=main_menu(is_admin=(message.from_user.id == ADMIN_ID)),
     )
-
-
-@router.message(lambda m: m.text == "✉ Написать в поддержку")
-async def support_start(message: types.Message, state: FSMContext):
-    await state.set_state(MasterForm.waiting_name)
-
-    await message.answer(
-        "Опишите проблему или вопрос.\n\n"
-        "Сообщение будет отправлено администратору.",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-
-@router.message(MasterForm.waiting_name)
-async def signup_name(message: types.Message, state: FSMContext):
-    await state.update_data(name=message.text)
-    await state.set_state(MasterForm.waiting_phone)
-    await message.answer("Введите номер телефона:")
-
-
-@router.message(MasterForm.waiting_phone)
-async def signup_phone(message: types.Message, state: FSMContext):
-    data = await state.get_data()
-
-    # Сохраняем подписчика
-    add_subscriber(message.from_user.id, data["name"], message.text)
-
-    await message.answer(
-        "✅ Вы подписаны на мастер-классы.\nМы будем с вами на связи для дальнейших действий.",
-        reply_markup=bottom_kb(is_admin=False),
-    )
-    await state.clear()
